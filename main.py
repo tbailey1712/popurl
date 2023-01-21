67095117d5307e2654404636d3eb10d3d911e0bd
tbailey1712 <tony.bailey@gmail.com>
2023-01-21 17:47:02 -0600

    fix var names

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from flask import escape, abort, redirect, render_template
 from google.cloud import datastore
-
 import functions_framework
-import re
 
 datastore_client = datastore.Client()
 
 @functions_framework.http
 def submit(request):
-    print("#### Received Form ")
-
     query = datastore_client.query(kind="urls")
     entities = list(query.fetch())
     for entity in entities:
@@ -40,14 +36,14 @@ def main(request):
 
         query = datastore_client.query(kind="urls")
         # Fetch the first entity
-        first_word = list(query.fetch(limit=1))
+        results = list(query.fetch(limit=1))
 
-        if first_word:
-            first_word = first_word[0]
+        if results:
+            first_url = results[0]
             # Delete the first word
-            datastore_client.delete(first_word.key)
-            print(f"{first_word['url']} deleted.")
-            return first_word['url']
+            datastore_client.delete(first_url.key)
+            print(f"{first_url['url']} deleted.")
+            return redirect(first_url['url'])
         else:
             # If the kind is empty
             print(f"No URLS!")        
@@ -58,8 +54,3 @@ def main(request):
     else:
         return abort(405)
 
-#app = Flask(__name__)
-
-#@app.route('/')
-#def redirect_url():
-#    return redirect("https://www.google.com")
